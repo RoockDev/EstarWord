b119ffa30bbcc45c85cb36f041a5b9d7b166a9a8
Sergio Zarcero Perez <zar0_31@outlook.com>
2025-10-20 22:03:01 +0200

    Nave Controller

--- app/Http/Controllers/NaveController.php
+++ app/Http/Controllers/NaveController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Nave;
+use App\Models\Piloto;
 use Illuminate\Http\Request;
 
 class NaveController extends Controller
@@ -25,6 +26,33 @@ class NaveController extends Controller
 
         return response()->json($nave, 201);
     }
+
+    /* PUT /api/naves/{nave} */
+    public function update(Request $request, Nave $nave){
+        $nave->update($request->all());
+        return response()->json($nave,200);
+    }
+
+    /* DELETE /api/naves/{nave} */
+    public function destroy(Nave $nave){
+        $nave->delete();
+        return response()->json(["Exito"=> "Nave eliminada correctamente"],204);
+    }
+
+    /**asignar piloto a nave */
+    public function asginarPiloto(Request $request, Nave $nave){
+        
+        $piloto = Piloto::find($request->piloto_id);
+        if (!$piloto) {
+            return response()->json(["Error" => "Piloto no encontrado"],404);
+        }
+
+        $nave->pilotos()->attach($piloto->id);
+        return response()->json(["succes" => "Piloto asinado correctamente"]);
+
+    }
+        
+    
 
 
 
--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/naves', [NaveController::class, 'index']);
 Route::get('/naves/{nave}', [NaveController::class, 'show']);
 Route::post('/naves',[NaveController::class, 'store']);
-Route::put('/naves', [NaveController::class],'update');
+Route::put('/naves/{nave}', [NaveController::class],'update');
 Route::delete('/naves/{nave}', [NaveController::class, 'destroy']);
 
